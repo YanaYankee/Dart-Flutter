dd7ab5bb31135e0647623895b85ebbfab8909f01
yanina.kononenko <yanina.kononenko@intellias.com>
2022-12-08 12:50:47 +0200

    Add answer and question classes

---
+++ lib/answer.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final Function selectHandler;
+  final String answerText;
+
+  Answer(this.selectHandler, this.answerText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(
+            Color.fromARGB(255, 237, 113, 157)
+            ),
+            foregroundColor: MaterialStateProperty.all(
+            Color.fromARGB(255, 255, 255, 255)
+            ),
+            ),
+        child: Text(answerText),
+        onPressed: selectHandler,
+      ),
+    );
+  }
+}
